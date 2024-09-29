7ebf7afd2103179171d9bfef5051058323cb312c
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-09-29 13:33:43 -0700

    Add files via upload
    first commit!!

---
+++ takatakatoe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  char board[3][3];
+  int wins = 0;
+  const int X_MOVE = 0;
+  const int O_MOVE = 1;
+  cout << "Enter a number!" ;
+  
+
+  return 0;
+
+}
