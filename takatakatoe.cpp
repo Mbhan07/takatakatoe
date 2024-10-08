6fa5e7cd1df08ea617850f4c9c99b38c91a19656
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-10-08 01:16:52 -0700

    Add files via upload

--- takatakatoe.cpp
+++ takatakatoe.cpp
@@ -2,14 +2,33 @@
 
 using namespace std;
 
+//intialize functions
+
+
+
+
+
+
 int main(){
 
   char board[3][3];
-  int wins = 0;
-  const int X_MOVE = 0;
-  const int O_MOVE = 1;
-  cout << "Enter a number!" ;
-  
+  int X_WINS = 0;
+  int O_WINS = 0;
+  char player = 'X';
+  bool stillPlaying  = true;
+  char row;
+  char col;
+
+  //while (stillPlaying){
+    
+    cout << "Player " << player << ", enter your row (1, 2, 3): ";
+    cin >> row;
+
+    cout << "Player " << player << ", enter your column (a, b, c): ";
+    cin >> col;
+    
+    
+    //}
 
   return 0;
 
