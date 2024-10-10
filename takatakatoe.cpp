aa03ea3ec433674688f5c4ef030dec25da460037
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-10-10 00:14:13 -0700

    Add files via upload

--- takatakatoe.cpp
+++ takatakatoe.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 //intialize functions
 
+void printBoard(int board[3][3]);
+
+/*void printBoard(char board[3][3]){
+  cout << "a \t b \t c";
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      board[i][j] = ' - ';
+   
+
+  }
+
+  }*/
 
 
 
@@ -18,18 +30,36 @@ int main(){
   bool stillPlaying  = true;
   char row;
   char col;
-
-  //while (stillPlaying){
     
     cout << "Player " << player << ", enter your row (1, 2, 3): ";
     cin >> row;
 
+
     cout << "Player " << player << ", enter your column (a, b, c): ";
     cin >> col;
-    
-    
-    //}
+    if (col = 'a'){
+      col = 1;
+    }else if (col = 'b'){
+      col = 2;
+    }else if (col = 'c'){
+      col = 3;
+    }else{
+      cout << "Please enter a valid character for column (a, b or c)";
+	cin >> col;
+    }
 
   return 0;
+
+}
+
+
+void printBoard(int board[3][3]){
+  cout << "a \t b \t c";
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      //      board[i][j] = '  ';
+    }
+
+  }
 
 }
